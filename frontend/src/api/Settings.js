91b14214b8692d45ef67a250ee8f2fdef712fbf4
Artyom Timofeyev <tema.timofeev20128@gmail.com>
2024-11-12 19:19:48 +0300

    add env for frontend

--- frontend/src/api/Settings.js
+++ frontend/src/api/Settings.js
@@ -1,4 +1,4 @@
-const HTTP_SCHEMA = "http";
-const HOST = "localhost:3001";
+const HTTP_SCHEMA = process.env.REACT_APP_API_HTTP_SCHEMA;
+const HOST = process.env.REACT_APP_API_HOST;
 
 export {HTTP_SCHEMA, HOST};
